e2435655b5124650c6cc4bb8e83fd819cabc9950
Pranav Laxman <plaxman81@gmail.com>
2022-12-10 02:30:29 -0500

    Forgot to add comments

--- day06/day06.py
+++ day06/day06.py
@@ -7,25 +7,29 @@ def main(window_size):
     end = window_size
 
     def add_to_chars(char):
+        # Increment count for char
         if not chars.get(char):
             chars[char] = 1
         else:
             chars[char] += 1
 
-
+    # Initilize chars with the starting window
     for char in input[start:end]:
         add_to_chars(char)
 
     while True:
+        # Each key in chars is a unique character
         if len(chars) == window_size:
             break
 
+        # Decrement the count for the character at the start of the window and move the start up
         if chars[input[start]] == 1:
             chars.pop(input[start])
         else:
             chars[input[start]] -= 1
         start += 1
 
+        # Add the next character in the string and move end of the window up
         add_to_chars(input[end])
         end += 1
 
